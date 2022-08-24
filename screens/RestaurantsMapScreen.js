7442fddc410769d075811685114a0bca0cc593a4
nass <nass@debian.nass>
2022-08-24 15:28:52 +0200

    save

--- screens/RestaurantsMapScreen.js
+++ screens/RestaurantsMapScreen.js
@@ -15,6 +15,7 @@ import {FlatList} from 'react-native-gesture-handler'
 //import { Reward } from './Offers'
 import Reward from '../components/Reward'
 import { getDistanceFromLatLonInKm } from '../utils'
+import RestaurantMarkers from '../components/restaurantComponents/RestaurantMarkers'
 
 
 
@@ -30,21 +31,6 @@ export default function RestaurantsMapScreen({route, navigation}) {
   const { width, height } = useWindowDimensions();
 
 
-  const _map = useRef(null)
-
-  const restaurantsRef = useRef(null)
-
-  const [visible, setVisible] = useState(true)
-
-  const [scrollEnabled, setScrollEnabled] = useState(false)
-
-  const [offset, setOffset] = useState(0)
-  const [direction, setDirection] = useState("")
-  
-   
- 
-  
- 
   useEffect(()=>{
     location().then((loc)=>{
       setMyLocation(loc)
@@ -82,20 +68,9 @@ export default function RestaurantsMapScreen({route, navigation}) {
 
       </MapView>
 
-      <View style={{...styles.header, width: width,}}>
+     
          <ArrowBack navigation={navigation}/>
-         <View style={styles.searchbar}>
-           <SearchBar />
-         </View>
-      </View>
-       
-
-     
-
-     
-     {!visible && <RestaurantsView restaurantsRef={restaurantsRef} restaurantDataSort={restaurantDataSort} setFocusFunction={setFocusFunction}
-     focus={focus} _map={_map} width={width} horizontal={true}/>}
-                 
+          
     </View>
   )
 }
@@ -106,59 +81,11 @@ export default function RestaurantsMapScreen({route, navigation}) {
 
 const styles = StyleSheet.create({
 
-  header: {
-    position: "absolute",
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    paddingBottom: 10,
-    zIndex: 1
-},
-   searchbar: {flex: 1, marginHorizontal: 10},
 
-   categories: {
-    marginBottom: 10
-   },
-
-   bubble: {
-      flexDirection: "row",
-      alignSelf: "flex-start",
-      backgroundColor: "#fff",
-      borderRadius: 6,
-      borderColor: '#ccc',
-      borderWidth: 0.5,
-      padding: 15,
-   },
-   bubbleName: {
-     fontSize: 16,
-     marginBottom: 5,
-
-   },
-  activityIndicator: {
-    backgroundColor: 'black',
-    position: 'absolute',
-    opacity: 0.6,
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
-    width: "100%",
-    
-   },
    
-    restaurant_marker_shadow:{
-    backgroundColor: "grey", 
-    top:0,left:0,
-    width:43, height: 43
-    },
-    
-
-  flatlist: {
-    position: "absolute",
-    //backgroundColor: "red", 
-    bottom: 70
-  },
    
+
+  
   restaurantsContainer: 
     {
       //backgroundColor: "red",
