8f6653513812c594eec8176ee00150e841a2b273
nass <cabliveer@gmail.com>
2022-09-03 16:34:49 +0200

    dgdg

--- components/OrderItem.js
+++ components/OrderItem.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import { language, currency } from '../../global'
+import { language, currency } from '../global'
 import { MaterialIcons } from '@expo/vector-icons';
 
 
