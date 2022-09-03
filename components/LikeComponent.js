9d0dcc6fc6ff698fd89f8f0a3b77de40d225a859
nass <cabliveer@gmail.com>
2022-09-04 00:35:06 +0200

    dg

--- components/LikeComponent.js
+++ components/LikeComponent.js
@@ -1,4 +1,4 @@
-import { AntDesign } from "@expo/vector-icons"
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons"
 import { useState } from "react"
 import { TouchableOpacity } from "react-native"
 import { colors, icon } from "../global"
