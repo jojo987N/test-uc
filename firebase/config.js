04023a06ce729cea538325a2a4aca0130af32a1d
nass <cabliveer@gmail.com>
2022-09-03 17:50:19 +0200

    fs

--- firebase/config.js
+++ firebase/config.js
@@ -2,17 +2,26 @@ import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
 
-    apiKey: {/* Your Firebase config here*/ },
+    // apiKey: {/* Your Firebase config here*/ },
 
-    authDomain: {/* Your Firebase config here*/ },
+    // authDomain: {/* Your Firebase config here*/ },
 
-    projectId: {/* Your Firebase config here*/ },
+    // projectId: {/* Your Firebase config here*/ },
 
-    storageBucket: {/* Your Firebase config here*/ },
+    // storageBucket: {/* Your Firebase config here*/ },
 
-    messagingSenderId: {/* Your Firebase config here*/ },
+    // messagingSenderId: {/* Your Firebase config here*/ },
 
-    appId: {/* Your Firebase config here*/ }
+    // appId: {/* Your Firebase config here*/ }
+
+    apiKey: "AIzaSyDrKqjM-fKGWBqj0-wpOOrIbeVlViEW-3c",
+    authDomain: "good-food-c84d4.firebaseapp.com",
+    projectId: "good-food-c84d4",
+    storageBucket: "good-food-c84d4.appspot.com",
+    messagingSenderId: "716731554402",
+    appId: "1:716731554402:web:bc8a1748f6cdd6885e8f3b",
+    measurementId: "G-VLK10R4D2P"
+
 
 };
 
