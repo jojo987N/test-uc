6c7846d1d570defea9935476ea16d6ac00090dd2
nass <cabliveer@gmail.com>
2022-09-03 15:32:13 +0200

    dg

--- screens/SearchResults.js
+++ screens/SearchResults.js
@@ -1,7 +1,7 @@
 import { View, Text, FlatList, StyleSheet} from 'react-native'
 import React, { useEffect, useState } from 'react'
 
- import { getRestaurantsFromFirebase } from '../firebase'
+ import { getRestaurantsFromFirebase } from '../firebase/utils'
 import {RestaurantImage, RestaurantInfo} from '../components/home/RestaurantItems'
 import Loader from './Loader'
 
