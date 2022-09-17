1b938c425527a9f1fbad7b1ae64e04e057d53c5f
nass <cabliveer@gmail.com>
2022-09-17 08:20:41 +0200

    sf

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,44 +1,71 @@
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar} from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Modal, StatusBar} from 'react-native'
+import React, { useState } from 'react'
 import { ArrowBack } from '../components/restaurantDetail/About'
 import { grey1 } from '../global'
 import { AntDesign } from '@expo/vector-icons'
+import { stripePayment } from '../utils'
+import { useStripe } from '@stripe/stripe-react-native';
+import AddFunds from './AddFunds'
+import {language, currency}  from '../global'
 
-export default function Wallet({navigation}) {
-  return (
-      <View style={styles.container}>
-          <ArrowBack navigation={navigation} />
-          <Text style={styles.title}>Wallet</Text>
-          <View style={styles.cash}>
-              <View style={styles.cashContainer}>
-                  <View style={styles.cashTexts}>
-                      <Text style={styles.cashText}>Cash</Text>
-                      <Text style={styles.cashNumber}>$0.00</Text>
-                  </View>
-                  <AntDesign name="right" size={20} color="grey" style={styles.right} />
-              </View>
-          </View>
-          <TouchableOpacity style={styles.textContainer} onPress={()=>navigation.push("AddCard")}>
-              <Text style={styles.text}>Add payment method</Text>
-          </TouchableOpacity>
-          <View style={styles.textContainer}>
-              <Text style={styles.text}>Connect Account</Text>
-          </View>
-          <View style={styles.textContainer}>
-              
-              <Text style={styles.text}>Vouchers</Text>
-          </View>
-          <View style={styles.textContainer}>
-              <Text style={styles.text}>Add voucher code </Text>
-          </View>
-      </View>
-  )
+
+
+export default function Wallet({ navigation }) {
+
+    const stripe = useStripe();
+    const [modalVisible, setModalVisible] = useState(false)
+    const [amount, setAmount] = useState(0.00)
+    return (
+        <>
+            <Modal
+                animationType='slide'
+                visible={modalVisible}
+                // transparent={true}
+                onRequestClose={() => setModalVisible(false)}>
+                {/* {checkoutModalContent()} */}
+                <AddFunds setModalVisible={setModalVisible}  setAmount={setAmount}/>
+            </Modal>
+            <View style={styles.container}>
+                <ArrowBack navigation={navigation} />
+                <Text style={styles.title}>Wallet</Text>
+                <View style={styles.cash}>
+                    <View style={styles.cashContainer}>
+                        <View style={styles.cashTexts}>
+                            <Text style={styles.cashText}>Cash</Text>
+                            <Text style={styles.cashNumber}>{amount.toLocaleString(language, {
+        style: "currency",
+        currency: currency
+      })}</Text>
+                        </View>
+                        <AntDesign name="right" size={20} color="grey" style={styles.right} />
+                    </View>
+                </View>
+                <TouchableOpacity style={styles.textContainer} onPress={() => {
+                    // navigation.push("AddFunds")
+                    // stripePayment(stripe)
+                    setModalVisible(true)
+                }}>
+                    <Text style={styles.text}>Add Funds to your wallet</Text>
+                </TouchableOpacity>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>Connect Account</Text>
+                </View>
+                <View style={styles.textContainer}>
+
+                    <Text style={styles.text}>Vouchers</Text>
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>Add voucher code </Text>
+                </View>
+            </View>
+        </>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-      marginHorizontal: 10,
-      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        marginHorizontal: 10,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
 
     },
     title: {
@@ -47,40 +74,40 @@ const styles = StyleSheet.create({
         marginBottom: 30
     },
     cashContainer: {
-       flexDirection: "row",
-       justifyContent: "space-between",
-       alignItems: "center"
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
     },
     cash: {
-      backgroundColor: grey1,
-      borderRadius: 10,
-      marginHorizontal: 10,
-      elevation: 5,
-      
+        backgroundColor: grey1,
+        borderRadius: 10,
+        marginHorizontal: 10,
+        elevation: 5,
+
     },
     cashTexts: {
-       marginLeft: 20 , 
-       paddingVertical: 25,
-       paddingBottom: 30
+        marginLeft: 20,
+        paddingVertical: 25,
+        paddingBottom: 30
     },
     cashNumber: {
         fontSize: 35,
         fontWeight: "bold",
         //paddingVertical: 30,
-       
+
     },
     cashText: {
-        
+
     },
     right: {
-     marginRight: 10
+        marginRight: 10
     },
-    textContainer:{
+    textContainer: {
         borderBottomWidth: 0.5
     },
     text: {
-     paddingVertical: 30,
-     marginLeft: 10,
-     color:"green"
+        paddingVertical: 30,
+        marginLeft: 10,
+        color: "green"
     }
 })
