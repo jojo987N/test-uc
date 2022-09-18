dfa778a019cff5e172d2dbd7161814ad71f02b39
nass <cabliveer@gmail.com>
2022-09-18 05:52:40 +0200

    save

--- components/Size.js
+++ components/Size.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Text, View, StyleSheet } from "react-native"
 import { CheckBox } from "react-native-elements"
+import { useDispatch } from "react-redux"
 import { currency, language } from "../global"
 
 const Size = ({ food, restaurant}) => {
