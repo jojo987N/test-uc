1262d4c55ae184f54238e1ae0b8ba7a333010d1e
Your Name <you@example.com>
2022-09-30 02:46:31 +0000

    dg

--- components/DisplayMapview.js
+++ components/DisplayMapview.js
@@ -1,34 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import MapView, { Marker, Polyline, PROVIDER_GOOGLE } from 'react-native-maps' 
-import MapViewDirections from 'react-native-maps-directions';
-import { CustomMarker, DisplayMapviewDirections } from '../screens/OrderRequest'
-const DisplayMapview = ({userLocation, mapRef, apikey, restaurant, height}) => {
+import React from "react";
+import MapView, { Polyline, PROVIDER_GOOGLE } from "react-native-maps";
+
+const DisplayMapview = ({
+  userLocation,
+  mapRef,
+  apikey,
+  restaurant,
+  height,
+}) => {
   return (
     <MapView
-        provider={PROVIDER_GOOGLE}
-        ref={mapRef}
-        initialRegion={{
-          // ...userLocation,
-          latitude: restaurant.lat,
-          longitude: restaurant.lng,
-          latitudeDelta: 0.09,
-          longitudeDelta: 0.04 
-        }}
-       style={{height: height?height:200, width: "100%",
-       }} showsUserLocation={true}
-       >
-<Polyline coordinates={[{
-         latitude: userLocation.latitude,
-         longitude: userLocation.longitude
-       },
-        {
-          // latitude: 48.904634,
-          // longitude: 2.432682
-
-          latitude: restaurant.lat,
-          longitude: restaurant.lng
-        }]}
+      provider={PROVIDER_GOOGLE}
+      ref={mapRef}
+      initialRegion={{
+        ...userLocation,
+        latitudeDelta: 0.18,
+        longitudeDelta: 0.08,
+      }}
+      style={{
+        height: height ? height : 200,
+        width: "100%",
+      }}
+      showsUserLocation={true}
+    >
+      <Polyline
+        coordinates={[
+          {
+            latitude: userLocation.latitude,
+            longitude: userLocation.longitude,
+          },
+          {
+            latitude: 48.904634,
+            longitude: 2.432682,
+          },
+        ]}
         strokeColor="#000"
         strokeColors={[
           "#7F0000",
@@ -36,11 +41,12 @@ const DisplayMapview = ({userLocation, mapRef, apikey, restaurant, height}) => {
           "#B24112",
           "#E5845C",
           "#238C23",
-          "#7F0000"
+          "#7F0000",
         ]}
         strokeWidth={6}
-        />
-       </MapView>
-  )
-}
-export default DisplayMapview
+      />
+    </MapView>
+  );
+};
+
+export default DisplayMapview;
