a4c423097941fbeb5f04c95afe36adbe956ba960
Your Name <you@example.com>
2022-09-30 02:45:10 +0000

    sf

--- components/DisplayPreferences.js
+++ components/DisplayPreferences.js
@@ -1,39 +1,50 @@
-import { View, Text, StyleSheet} from 'react-native'
-import React, { useState } from 'react'
-import { menuDetailedData } from '../data'
-import { CheckBox } from 'react-native-elements'
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { menuDetailedData } from "../data";
+import { CheckBox } from "react-native-elements";
+
 export default function DisplayPreferences() {
+  const [focus, setFocus] = useState(
+    new Array(restaurantData.length).fill({
+      backgroundColor: "white",
+      color: "black",
+      zIndex: 1,
+    })
+  );
+
   return (
-      <View style={styles.container}>
-          {menuDetailedData[0].preferenceTitle.map((preft, index) => {
+    <View style={styles.container}>
+      {menuDetailedData[0].preferenceTitle.map((preft, index) => {
+        return (
+          <View key={index}>
+            <Text style={styles.title}>{preft.toUpperCase()}</Text>
+            {menuDetailedData[0].preferenceData[index].map((prfd, i, tab) => {
+              const [ptab, setPtab] = useState(tab);
+
               return (
-                  <View key={index}>
-                      <Text style={styles.title}>{preft.toUpperCase()}</Text>
-                    {menuDetailedData[0].preferenceData[index].map((prfd, i, tab)=>{
-                            const [ptab, setPtab] = useState(tab)
-                            return (
-                                <CheckBox
-                                key={i}
-                                title={prfd.name}
-                                checked={ptab[i].checked}
-                                onPress={() => {
-                                    tab[i].checked= true
-                                }}
-                            />
-                            )
-                    })}
-                  </View>
-              )
-          })}
-      </View>
-  )
+                <CheckBox
+                  key={i}
+                  title={prfd.name}
+                  checked={ptab[i].checked}
+                  onPress={() => {
+                    tab[i].checked = true;
+                  }}
+                />
+              );
+            })}
+          </View>
+        );
+      })}
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
-    marginBottom : 20
+    marginBottom: 20,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
   },
-})
+});
