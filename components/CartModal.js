47191435af1e35b059d190fb29398bb81ac3f864
Your Name <you@example.com>
2022-09-30 03:11:55 +0000

    save

--- components/CartModal.js
+++ components/CartModal.js
@@ -2,14 +2,16 @@ import { View, Text, Modal } from 'react-native'
 import React from 'react'
 import Cart from './Cart'
 
-export default function CartModal({modalVisible, setModalVisible, restaurantName, setViewCartButton}) {
+
+export default function CartModal({ modalVisible, setModalVisible, restaurantName, setViewCartButton }) {
+
   return (
     <Modal
-    animationType='slide'
-    visible={modalVisible}
-    transparent={true}
-    onRequestClose={() => setModalVisible(false)}>
-    <Cart restaurantName={restaurantName} setViewCartButton={setViewCartButton} setModalVisible={setModalVisible}/>
-   </Modal>
+      animationType='slide'
+      visible={modalVisible}
+      transparent={true}
+      onRequestClose={() => setModalVisible(false)}>
+      <Cart restaurantName={restaurantName} setViewCartButton={setViewCartButton} setModalVisible={setModalVisible} />
+    </Modal>
   )
 }
